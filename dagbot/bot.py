ba3fb4b0d85028f3f34ecc56cece838ae7a02061
Daggy1234 <arnav.jindal7@gmail.com>
2021-01-07 16:26:34 +0000

    stop

--- dagbot/bot.py
+++ dagbot/bot.py
@@ -24,7 +24,6 @@ async def get_prefix(bot, message):
        if e["server_id"] == str(g_id):
            prefix = e["command_prefix"]
            break
-    return "dev "
     return commands.when_mentioned_or(prefix)(bot, message)
 
 
